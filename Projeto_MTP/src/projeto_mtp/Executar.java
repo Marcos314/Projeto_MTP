ca41501d341268b444d44130f8f8b462ae9f08da
Marcos Wesley <36406574+Marcos314@users.noreply.github.com>
2018-11-22 11:11:27 -0200

    Add files via upload

---
+++ Projeto_MTP/src/projeto_mtp/Executar.java
@@ -0,0 +1,12 @@
+
+package projeto_mtp;
+
+public class Executar {
+    
+    public static void main (String[] args){        
+        
+        new Interface1();
+        
+    }
+    
+}
---
+++ Projeto_MTP/src/projeto_mtp/Projeto_MTP.java
@@ -0,0 +1,10 @@
+
+package projeto_mtp;
+
+public class Projeto_MTP {
+
+ static void main(String[] args) {
+        
+    }
+    
+}
